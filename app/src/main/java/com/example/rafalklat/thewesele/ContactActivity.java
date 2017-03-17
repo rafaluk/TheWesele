33deeb77be11e6fe4fc5f0545696bb8e310e3845
Rafał Klat <rafal.klat@gmail.com>
2017-03-17 11:51:52 +0100

    nowe activity contact

--- app/src/main/java/com/example/rafalklat/thewesele/ContactActivity.java
+++ app/src/main/java/com/example/rafalklat/thewesele/ContactActivity.java
@@ -1,8 +1,13 @@
 package com.example.rafalklat.thewesele;
 
-/**
- * Created by rafal.klat on 17.03.2017.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class ContactActivity {
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
 }
