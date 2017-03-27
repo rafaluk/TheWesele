100365861d7bf6770714a17f5a01caeb39b26a6e
rafaluk <rafal.klat@gmail.com>
2017-03-28 01:23:41 +0200

    refactor: lepszy sposob liczenia daty

--- app/src/main/java/com/example/rafalklat/thewesele/MainFragment.java
+++ app/src/main/java/com/example/rafalklat/thewesele/MainFragment.java
@@ -4,6 +4,7 @@ package com.example.rafalklat.thewesele;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +21,7 @@ public class MainFragment extends Fragment {
 //TODO zamiana tekstow na stringi i angielskie
     Button details_button;
     Button contact_button;
+    TextView months;
     TextView days;
     TextView hours;
     TextView minutes;
@@ -28,23 +30,36 @@ public class MainFragment extends Fragment {
     final long WEDDING_TIME = 1502550000000L;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // TODO: PRZEBUDOWAĆ CAŁY WIDOK
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        days = (TextView) rootView.findViewById(R.id.months);
+        Calendar weddingTime = Calendar.getInstance();
+        weddingTime.set(2017, 7, 12, 18, 0, 0); //miesiac -1, godzina +1
+
+        months = (TextView) rootView.findViewById(R.id.months);
+        days = (TextView) rootView.findViewById(R.id.days);
         hours = (TextView) rootView.findViewById(R.id.hours);
         minutes = (TextView) rootView.findViewById(R.id.minutes);
         seconds = (TextView) rootView.findViewById(R.id.seconds);
 
-        new CountDownTimer(WEDDING_TIME - Calendar.getInstance().getTimeInMillis(), 1000) {
+        new CountDownTimer(weddingTime.getTimeInMillis() - Calendar.getInstance().getTimeInMillis(), 1000) {
             public void onTick(long millisUntilFinished) {
                 //TODO zamienic na miesiace, a nie dni
-                days.setText("" + (millisUntilFinished / 1000 / 60 / 60 / 24));
-                hours.setText("" + (millisUntilFinished / 1000 / 60 / 60) % 24);
-                minutes.setText("" + (millisUntilFinished / 1000 / 60) % 60);
-                seconds.setText("" + (millisUntilFinished / 1000) % 60);
+
+                Calendar roznica = Calendar.getInstance();
+                roznica.setTimeInMillis(millisUntilFinished);
+
+//                Log.d("left: ",Integer.toString(miechy)+"mc, "+Integer.toString(dniechy)+"d, "+Integer.toString(godzinki)+"h.");
+//TODO sprawdzic, ktory sposob jest wydajniejszy
+                months.setText(Integer.toString(roznica.get(Calendar.MONTH)));
+                days.setText(Integer.toString(roznica.get(Calendar.DAY_OF_MONTH)));
+                hours.setText(Integer.toString(roznica.get(Calendar.HOUR_OF_DAY)));
+                minutes.setText(Integer.toString(roznica.get(Calendar.MINUTE)));
+                seconds.setText(Integer.toString(roznica.get(Calendar.SECOND)));
+//                days.setText("" + (millisUntilFinished / 1000 / 60 / 60 / 24));
+//                hours.setText("" + (millisUntilFinished / 1000 / 60 / 60) % 24);
+//                minutes.setText("" + (millisUntilFinished / 1000 / 60) % 60);
+//                seconds.setText("" + (millisUntilFinished / 1000) % 60);
             }
 
             public void onFinish() {
